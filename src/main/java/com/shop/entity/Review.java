7153099d49d3b17caf3f39149d22eb061ac7f5cc
18-cocp-126g <18-cocp-126g@18-cocp-126>
2023-03-10 12:46:30 +0900

    entity 문법오류 수정

--- src/main/java/com/shop/entity/Review.java
+++ src/main/java/com/shop/entity/Review.java
@@ -1,7 +1,24 @@
 package com.shop.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Review {
 	
 	@Id
@@ -9,7 +26,8 @@ public class Review {
 	
 	private String rTitle;
 	private String rContent;
-	private String rWirter;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member rWirter;
 	private String rImg;
 	
 	public void changerTitle(String title) {
